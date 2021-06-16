5d785b0ecdda26bd7652d15cd601c50f8c055975
Willem Jacobs <wamj58@gmail.com>
2021-06-15 21:12:42 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Global variabls
+
+// JS to HTML links
+
+// Constructor for Product
+// function Product(name, image) {
+//   this.name = name;
+//   this.displayImage = image;
+//   this.clickCount = 0;
+//   this.showCount = 0;
+//   allProducts.push(this);
+// }
+
+// Extend string object add method to capitalize first letter
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+// Functions
+
+// Start site
