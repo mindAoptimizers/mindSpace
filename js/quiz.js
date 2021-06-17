d4feb715fc10692d7528b361d3b253ccddd30aa3
Daviey52 <davidkariska@gmail.com>
2021-06-17 10:22:02 -0700

    working quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,18 +1,26 @@
 'use strict'
-
+// Correct Answers Array
 const answers = ['B', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'A', 'A'];
 
-const submitButton = document.getElementById('results');
-const quizForm = document.querySelector('.form');
+const result = document.querySelector('.result');
+const quizForm = document.querySelector('.quizForm');
 
 quizForm.addEventListener('submit', function (e) {
   e.preventDefault();
   let score = 0;
-  const userAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value, quizForm.q5.value, quizForm.q6.value, quizForm.q7.value, quizForm.q8.value, quizForm.q8.value, quizForm.q9.value, quiz.q10.value];
 
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]) {
-      score += 25;
+  //checking user input from the form.
+  const userAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value, quizForm.q5.value, quizForm.q6.value, quizForm.q7.value, quizForm.q8.value, quizForm.q9.value, quizForm.q10.value];
+
+  // verifying answers and increamenting scores
+  userAnswers.forEach(function (answer, index) {
+    if (answer === answers[index]) {
+      score += 10;
     }
 
   });
+  console.log(score);
+  result.querySelector('span').textContent = `${score}%`;
+  //scroll to method??
+
+})
