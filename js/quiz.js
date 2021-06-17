d5b4b88b85abbb6117be20661532e1a496b9014f
Daviey52 <davidkariska@gmail.com>
2021-06-16 21:21:45 -0700

    quiz-upadate

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,18 @@
 'use strict'
 
-const answers = [];
-const quizBox = document.getElementById('quiz-start');
-const resultsBox = document.getElementById('submit');
+const answers = ['B', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'A', 'A'];
+
 const submitButton = document.getElementById('results');
 const quizForm = document.querySelector('.form');
 
-quizForm.addEventListener('submit', function(e){
+quizForm.addEventListener('submit', function (e) {
   e.preventDefault();
   let score = 0;
-  const userAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value, quizForm.q5.value, quizForm.q6.value, quizForm.q7.value, quizForm.q8.value];
-  
-});
+  const userAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value, quizForm.q5.value, quizForm.q6.value, quizForm.q7.value, quizForm.q8.value, quizForm.q8.value, quizForm.q9.value, quiz.q10.value];
+
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+
+  });
